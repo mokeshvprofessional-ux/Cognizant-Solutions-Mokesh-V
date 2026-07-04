0fef2d836c99fa16a38fd247f7bdefee2857a79d
mokeshvprofessional-ux <mokeshv.professional@gmail.com>
2026-07-04 11:04:19 +0530

    Add files via upload

---
+++ Week 1/JUnit Basic/Ex 1.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class JUnitSetupTest {
+
+    @Test
+    public void testSetup() {
+        assertTrue(true);
+    }
+}
